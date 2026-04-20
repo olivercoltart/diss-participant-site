4b18c5498f2048968cdd365f70f10af6fc6f1e1b
olivercoltart <oli@coltart.org>
2026-04-20 18:21:55 +0100

    Fixing final page

--- app/complete/page.js
+++ app/complete/page.js
@@ -3,13 +3,8 @@ import Link from "next/link";
 export default function CompletePage() {
   return (
     <section className="card">
-      <h1>Complete</h1>
-      <p>Thank you — you’ve reached the end of this prototype flow.</p>
-      <nav className="nav">
-        <Link className="button" href="/welcome">
-          Restart
-        </Link>
-      </nav>
+      <h1>Thank you for taking part in this study!</h1>
+      <p>You can now close this tab</p>
     </section>
   );
 }
