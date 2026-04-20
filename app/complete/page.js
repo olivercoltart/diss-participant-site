3bd33252f0d557733f4ac2b18bfbee71c369efc2
olivercoltart <oli@coltart.org>
2026-04-20 18:31:16 +0100

    Cleaning up final page

--- app/complete/page.js
+++ app/complete/page.js
@@ -4,7 +4,7 @@ export default function CompletePage() {
   return (
     <section className="card">
       <h1>Thank you for taking part in this study!</h1>
-      <p>You can now close this tab</p>
+      <p>You can now close this tab.</p>
     </section>
   );
 }
